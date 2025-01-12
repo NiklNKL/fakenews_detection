5034c63102d06b8c5287941d07da13d039afa986
Jan Niklas Ewert <janniklas-ewert@web.de>
2025-01-12 23:19:11 +0100

    bug fixing commit

--- fakenews_classifier.py
+++ fakenews_classifier.py
@@ -12,6 +12,11 @@ import joblib
 import pandas as pd
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import PassiveAggressiveClassifier
+from PIL import Image
+import plotly.graph_objects as go
+import os
+from xml.etree import ElementTree as ET
+import plotly.express as px
 
 # Layout configuration
 st.set_page_config(layout="wide")
@@ -34,6 +39,10 @@ max_length = 512  # Adjust based on your model's requirements
 # Load SpaCy for preprocessing
 nlp = spacy.load('en_core_web_sm', disable=['tagger', 'parser', 'ner'])
 stopwords = nlp.Defaults.stop_words
+
+df = pd.read_parquet("data/train_cleaned.parquet")
+analysis_df = pd.read_parquet("data/analysis_df.parquet")
+n_grams_df = pd.read_parquet("data/precomputed_ngrams_combined.parquet")
 
 def preprocess_text_with_tracking(text):
     """
@@ -93,14 +102,34 @@ def preprocess_text_with_tracking(text):
 
     return processed_text, changes
 
-def get_bert_prediction(preprocessed_text):
-    # Prepare text into tokenized sequence
-    inputs = tokenizer(preprocessed_text, padding=True, truncation=True, max_length=max_length, return_tensors="pt").to("cuda")
-    # Perform inference with the model
-    outputs = model(**inputs)
-    # Get output probabilities by applying softmax
-    probs = outputs[0].softmax(1).detach().cpu().numpy()[0]
-    return probs
+def get_bert_prediction(preprocessed_text, chunk_size=512, overlap=50):
+    def chunk_text(text, tokenizer, chunk_size, overlap):
+        tokens = tokenizer(text, truncation=False)["input_ids"]
+        chunks = []
+        for i in range(0, len(tokens), chunk_size - overlap):
+            chunks.append(tokens[i:i + chunk_size])
+        return chunks
+
+    # Split the text into chunks
+    chunks = chunk_text(preprocessed_text, tokenizer, chunk_size, overlap)
+
+    # Initialize list to store probabilities
+    all_probs = []
+
+    for chunk in chunks:
+        # Prepare chunk into tokenized sequence
+        inputs = tokenizer(chunk, padding=True, truncation=True, max_length=chunk_size, return_tensors="pt").to("cuda")
+        # Perform inference with the model
+        outputs = model(**inputs)
+        # Get output probabilities by applying softmax
+        probs = outputs[0].softmax(1).detach().cpu().numpy()[0]
+        all_probs.append(probs)
+
+    # Aggregate probabilities (average pooling for example)
+    aggregated_probs = sum(all_probs) / len(all_probs)
+
+    return aggregated_probs
+
 
 # Preprocess input text and return the prediction
 def get_sklearn_prediction(model, processed_text):
@@ -116,121 +145,249 @@ def get_sklearn_prediction(model, processed_text):
         confidence = None  # No confidence if 'predict_proba' is not available
         probas = None
     return prediction, confidence
+
+def render_svg(svg_file_path, title):
+    """
+    Render an SVG file in a Streamlit app.
+    
+    Args:
+        svg_file_path (str): Path to the SVG file.
+    """
+    # Load the SVG file
+    tree = ET.parse(svg_file_path)
+    root = tree.getroot()
+    should_save = False
+    if 'width' in root.attrib:
+        del root.attrib['width']
+        should_save = True
+    if 'height' in root.attrib:
+        del root.attrib['height']
+        should_save = True
+    if should_save:
+        tree.write(svg_file_path)
+    
+    with open(svg_file_path, "r") as file:
+        svg_content = file.read()
+    st.markdown(f"""
+    <div style="text-align: center; margin: 20px;">
+        <h3 style="color: #4CAF50; font-family: Arial, sans-serif;">{title}</h3>
+        <div>
+            {svg_content}
+
+    """, unsafe_allow_html=True)
+
+def vocab_richness(label):
+    # Vocabulary richness distribution
+    temp_df = analysis_df[analysis_df['label'] == label]
+    fig_vocab = px.histogram(
+        temp_df, 
+        x='vocab_richness', 
+        color='label', 
+        nbins=50, 
+        title="Vocabulary Richness Distribution by Label",
+    )
+    st.plotly_chart(fig_vocab)
+    
+def article_length(label):
+    # Article length distribution
+    temp_df = analysis_df[analysis_df['label'] == label]
+    fig_length = px.histogram(
+        temp_df, 
+        x='text_length', 
+        color='label', 
+        nbins=50, 
+        title="Article Length Distribution by Label",
+    )
+    st.plotly_chart(fig_length)
+    
+def n_grams(label, ngram_size):
+    if label == 0:
+        temp_df = n_grams_df[n_grams_df['category'] == 'real']
+    else:
+        temp_df = n_grams_df[n_grams_df['category'] == 'fake']
+
+    # Filter data based on n-gram size
+    filtered_data = temp_df[temp_df['ngram_size'] == ngram_size]
+
+    # Create a bar plot
+    fig = px.bar(
+        filtered_data,
+        x='ngram',
+        y='count',
+        color='category',
+        title=f"Top {ngram_size}-grams in Real and Fake News",
+        labels={'ngram': 'N-gram', 'count': 'Frequency'},
+        height=600,
+    )
+
+    # Update layout for better display
+    fig.update_layout(
+        xaxis_title="N-gram",
+        yaxis_title="Frequency",
+        xaxis_tickangle=45,
+        xaxis_tickfont=dict(size=10),
+        yaxis_tickfont=dict(size=10),
+    )
+
+    # Display plot
+    st.plotly_chart(fig)
 # Sidebar content
 st.sidebar.image("assets/FHDW_Logo_RGB-01.svg.jpg", use_container_width=True)
 st.sidebar.header("Knowledge Engineering and Knowledge Representation")
 st.sidebar.text("This is a project that presents basic NLP methods to detect fake news using different models.")
+st.sidebar.write("")
+page = st.sidebar.radio("Go to", ["Fake News Detection", "Dataset Analysis", "Model Performance"])
 
+if page == "Dataset Analysis":
+    st.title("Dataset Analysis")
+    st.subheader("Real vs. Fake News: A Visual Comparison")
 
-col1, col2, col3 = st.columns([1, 2, 2])
-# Streamlit app
-with col2:
-    st.title("Fake News Detection")
+    # Split the page into two columns
+    real_news_col, fake_news_col = st.columns(2)
 
+    # Path to your SVG files
+    output_dir = "wordclouds"  # Adjust the directory path if needed
+    real_news_svg = os.path.join(output_dir, "real_news.svg")
+    fake_news_svg = os.path.join(output_dir, "fake_news.svg")
 
-
-# Create columns for better horizontal layout
-empty, input_col, steps_col = st.columns([1, 2, 2], gap="medium")
-
-# Text input
-with input_col:
-    user_input = st.text_area("Enter the text you want to analyze:", height=200)
-    button, dropdown = st.columns([1, 1])
-    with button:
-        button_pressed = st.button("Analyze")
-    with dropdown:
-        selected_models = st.multiselect("Select the models you want to use:", ["Logistic Regression", "Naive Bayes", "Decision Tree", "Passive-Aggressive", "BERT"])
-results = {}
-if button_pressed and user_input.strip() and selected_models:
-    preprocessed_text, changes = preprocess_text_with_tracking(user_input)
-    if "Logistic Regression" in selected_models:
-        logistic_regression_prediction = get_sklearn_prediction(log_reg_model, preprocessed_text)
-        results["Logistic Regression"] = logistic_regression_prediction
-    if "Naive Bayes" in selected_models:
-        naive_bayes_prediction = get_sklearn_prediction(naive_bayes_model, preprocessed_text)
-        results["Naive Bayes"] = naive_bayes_prediction
-    if "Decision Tree" in selected_models:
-        decision_tree_prediction = get_sklearn_prediction(decision_tree_model, preprocessed_text)
-        results["Decision Tree"] = decision_tree_prediction
-    if "Passive-Aggressive" in selected_models:
-        passive_aggressive_prediction = get_sklearn_prediction(passive_aggressive_model, preprocessed_text)
-        results["Passive-Aggressive"] = passive_aggressive_prediction
-    if "BERT" in selected_models:
-        probs = get_bert_prediction(preprocessed_text)
-        labels = [0,1]
-        prediction_label = labels[np.argmax(probs)]
-        results["BERT"] = prediction_label, probs[prediction_label]
     
-    # Display result in the result column
+
+    # Add Real News WordCloud on the left
+    with real_news_col:
+        render_svg(real_news_svg, "Real News WordCloud")
+
+
+    # Add Fake News WordCloud on the right
+    with fake_news_col:
+        render_svg(fake_news_svg, "Fake News WordCloud")
+
+    col_1, col_2, col_3 = st.columns([1, 2, 1])
+    with col_2:
+        st.title("N-grams and Most Frequent Words")
+        ngram_size = st.slider("Select n-gram size:", min_value=1, max_value=4, value=1)
+    real_news_col_2, fake_news_col_2 = st.columns(2)
+    with real_news_col_2:
+
+        n_grams(0, ngram_size)
+        vocab_richness(0)
+        article_length(0)
+
+    # Add Fake News WordCloud on the right
+    with fake_news_col_2:
+
+        n_grams(1, ngram_size)
+        vocab_richness(1)
+        article_length(1)
+if page == "Fake News Detection":
+    col1, col2, col3 = st.columns([1, 2, 2])
+    # Streamlit app
+    with col2:
+        st.title("Fake News Detection")
+
+
+
+    # Create columns for better horizontal layout
+    empty, input_col, steps_col = st.columns([1, 2, 2], gap="medium")
+
+    # Text input
     with input_col:
-        st.subheader("Prediction Result")
+        user_input = st.text_area("Enter the text you want to analyze:", height=200)
+        button, dropdown = st.columns([1, 1])
+        with button:
+            button_pressed = st.button("Analyze")
+        with dropdown:
+            selected_models = st.multiselect("Select the models you want to use:", ["Logistic Regression", "Naive Bayes", "Decision Tree", "Passive-Aggressive", "BERT"])
+    results = {}
+    if button_pressed and user_input.strip() and selected_models:
+        preprocessed_text, changes = preprocess_text_with_tracking(user_input)
+        if "Logistic Regression" in selected_models:
+            logistic_regression_prediction = get_sklearn_prediction(log_reg_model, preprocessed_text)
+            results["Logistic Regression"] = logistic_regression_prediction
+        if "Naive Bayes" in selected_models:
+            naive_bayes_prediction = get_sklearn_prediction(naive_bayes_model, preprocessed_text)
+            results["Naive Bayes"] = naive_bayes_prediction
+        if "Decision Tree" in selected_models:
+            decision_tree_prediction = get_sklearn_prediction(decision_tree_model, preprocessed_text)
+            results["Decision Tree"] = decision_tree_prediction
+        if "Passive-Aggressive" in selected_models:
+            passive_aggressive_prediction = get_sklearn_prediction(passive_aggressive_model, preprocessed_text)
+            results["Passive-Aggressive"] = passive_aggressive_prediction
+        if "BERT" in selected_models:
+            probs = get_bert_prediction(preprocessed_text)
+            labels = [0,1]
+            prediction_label = labels[np.argmax(probs)]
+            results["BERT"] = prediction_label, probs[prediction_label]
         
-        # Count predictions
-        count_0 = sum(1 for value in results.values() if value[0] == 0)
-        count_1 = sum(1 for value in results.values() if value[0] == 1)
-
-        # Display success or error message based on the majority prediction
-        if count_0 > count_1:
-            st.success("This news is probably reliable. You should still verify the information however.")
-            st.write("Number of models predicting reliable news:", count_0 ," of ", len(results))
-        else:
-            st.error("This could be fake news! Please verify the information before sharing.")
-            st.write("Number of models predicting fake news:", count_1 ," of ", len(results))
-        
-        st.write("Individual Model Predictions:")
-        prediction_data = []
-        for key, value in results.items():
-            prediction = "Fake" if value[0] == 1 else "Real"
-            confidence = f"{value[1] * 100:.2f}%" if value[1] is not None else "N/A"
-            prediction_data.append([key, prediction, confidence])
-
-        prediction_df = pd.DataFrame(prediction_data, columns=["Model Name", "Prediction", "Confidence"])
-        st.table(prediction_df)
+        # Display result in the result column
+        with input_col:
+            st.subheader("Prediction Result")
             
+            # Count predictions
+            count_0 = sum(1 for value in results.values() if value[0] == 0)
+            count_1 = sum(1 for value in results.values() if value[0] == 1)
 
-        # Prepare data for the Plotly bar chart
-        model_names = []
-        real_probs = []
-        fake_probs = []
-
-        for model_name, (prediction, confidence) in results.items():
-            model_names.append(model_name)
-            if prediction == 0:
-                real_probs.append(confidence)
-                fake_probs.append(1 - confidence if confidence is not None else None)
+            # Display success or error message based on the majority prediction
+            if count_0 > count_1:
+                st.success("This news is probably reliable. You should still verify the information however.")
+                st.write("Number of models predicting reliable news:", count_0 ," of ", len(results))
             else:
-                real_probs.append(1 - confidence if confidence is not None else None)
-                fake_probs.append(confidence)
+                st.error("This could be fake news! Please verify the information before sharing.")
+                st.write("Number of models predicting fake news:", count_1 ," of ", len(results))
+            
+            st.write("Individual Model Predictions:")
+            prediction_data = []
+            for key, value in results.items():
+                prediction = "Fake" if value[0] == 1 else "Real"
+                confidence = f"{value[1] * 100:.2f}%" if value[1] is not None else "N/A"
+                prediction_data.append([key, prediction, confidence])
 
-        # Dynamic Plotly bar chart for model confidence
-        fig = go.Figure(data=[
-            go.Bar(name='Real', x=model_names, y=real_probs, text=[f"{p*100:.2f}%" if p is not None else "N/A" for p in real_probs], textposition='auto', marker_color="green"),
-            go.Bar(name='Fake', x=model_names, y=fake_probs, text=[f"{p*100:.2f}%" if p is not None else "N/A" for p in fake_probs], textposition='auto', marker_color="red")
-        ])
-        fig.update_layout(
-            title="Model Confidence",
-            yaxis=dict(title="Confidence Level", range=[0, 1]),
-            xaxis=dict(title="Models"),
-            barmode='group',
-            plot_bgcolor="rgba(0,0,0,0)",
-            width=600
-        )
-        st.plotly_chart(fig)
+            prediction_df = pd.DataFrame(prediction_data, columns=["Model Name", "Prediction", "Confidence"])
+            st.table(prediction_df)
+                
 
-    # Additional statistics about the text in the stats column
-    with steps_col:
-        st.subheader("Text Statistics")
-        text_length = len(user_input)
-        word_count = len(user_input.split())
+            # Prepare data for the Plotly bar chart
+            model_names = []
+            real_probs = []
+            fake_probs = []
 
-        st.write(f"**Character Count:** {text_length}")
-        st.write(f"**Word Count:** {word_count}")
+            for model_name, (prediction, confidence) in results.items():
+                model_names.append(model_name)
+                if prediction == 0:
+                    real_probs.append(confidence)
+                    fake_probs.append(1 - confidence if confidence is not None else None)
+                else:
+                    real_probs.append(1 - confidence if confidence is not None else None)
+                    fake_probs.append(confidence)
 
-        # Display preprocessing changes
-        st.subheader("Preprocessing Changes")
-        if changes:
-            for step, change in enumerate(changes, 1):
-                st.write(f"{step}. {change}")
-        else:
-            st.write("No changes were made during preprocessing.")
+            # Dynamic Plotly bar chart for model confidence
+            fig = go.Figure(data=[
+                go.Bar(name='Real', x=model_names, y=real_probs, text=[f"{p*100:.2f}%" if p is not None else "N/A" for p in real_probs], textposition='auto', marker_color="green"),
+                go.Bar(name='Fake', x=model_names, y=fake_probs, text=[f"{p*100:.2f}%" if p is not None else "N/A" for p in fake_probs], textposition='auto', marker_color="red")
+            ])
+            fig.update_layout(
+                title="Model Confidence",
+                yaxis=dict(title="Confidence Level", range=[0, 1]),
+                xaxis=dict(title="Models"),
+                barmode='group',
+                plot_bgcolor="rgba(0,0,0,0)",
+                width=600
+            )
+            st.plotly_chart(fig)
+
+        # Additional statistics about the text in the stats column
+        with steps_col:
+            st.subheader("Text Statistics")
+            text_length = len(user_input)
+            word_count = len(user_input.split())
+
+            st.write(f"**Character Count:** {text_length}")
+            st.write(f"**Word Count:** {word_count}")
+
+            # Display preprocessing changes
+            st.subheader("Preprocessing Changes")
+            if changes:
+                for step, change in enumerate(changes, 1):
+                    st.write(f"{step}. {change}")
+            else:
+                st.write("No changes were made during preprocessing.")
 
