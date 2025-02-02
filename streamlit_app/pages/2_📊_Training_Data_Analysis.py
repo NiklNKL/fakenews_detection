3a05637f8a5b3668f28e2b8840900695aa394520
Jan Niklas Ewert <janniklas-ewert@web.de>
2025-02-02 23:38:14 +0100

    updated everything

--- streamlit_app/pages/2_📊_Training_Data_Analysis.py
+++ streamlit_app/pages/2_📊_Training_Data_Analysis.py
@@ -13,36 +13,39 @@ st.set_page_config(
     page_icon="📊",
     )
 
-
-
-# Get the root path of the project
 root_path = Path(__file__).resolve().parent.parent.parent 
 
 @st.cache_data
-def load_analysis_df():
-    return pd.read_parquet(f"{root_path}/data/analysis_df.parquet")
+def load_readability_metrics():
+    return pd.read_parquet(f"{root_path}/data/analysis_dataframes/readability_metrics.parquet")
+
+@st.cache_data
+def load_dependency_counts():
+    return pd.read_parquet(f"{root_path}/data/analysis_dataframes/dependency_counts.parquet")
+
+@st.cache_data
+def load_entity_counts():
+    return pd.read_parquet(f"{root_path}/data/analysis_dataframes/entity_counts.parquet")
 
 @st.cache_data
 def load_ngrams_df():
-    return pd.read_parquet(f"{root_path}/data/precomputed_ngrams_combined.parquet")
+    return pd.read_parquet(f"{root_path}/data/analysis_dataframes/ngrams.parquet")
 
 @st.cache_resource
-def load_svg(svg_file_path):
-    with open(svg_file_path, "r") as file:
-        return file.read()
+def load_svg():
+    root_dir = f"{root_path}/streamlit_app/assets/wordclouds"
+    dict_svg = {}
+    dir_files = {"real":"real_news.svg", "fake":"fake_news.svg"}
+    for label, file_path in dir_files.items():
+        with open(os.path.join(root_dir, file_path), "r") as file:
+            dict_svg[label] = file.read()
+    return dict_svg
 
-# Load data
-analysis_df = load_analysis_df()
+readability_metrics_df = load_readability_metrics()
+dependency_counts_df = load_dependency_counts()
+entity_counts_df = load_entity_counts()
 n_grams_df = load_ngrams_df()
-
-# Path to your SVG files
-output_dir = f"{root_path}/assets/wordclouds"  # Adjust the directory path if needed
-real_news_svg = os.path.join(output_dir, "real_news.svg")
-fake_news_svg = os.path.join(output_dir, "fake_news.svg")
-
-# Cached SVG content
-real_news_svg_content = load_svg(real_news_svg)
-fake_news_svg_content = load_svg(fake_news_svg)
+svg_dict = load_svg()
 
 
 def get_color(label):
@@ -60,14 +63,14 @@ def render_svg_inline(svg_content, title, label):
 
 def vocab_richness(label):
     # Vocabulary richness distribution
-    temp_df = analysis_df[analysis_df['label'] == label]
+    temp_df = readability_metrics_df[readability_metrics_df['label'] == label]
     if label == 0:
         title = "Real News"
     else:
         title = "Fake News"
     fig_vocab = px.histogram(
         temp_df, 
-        x='vocab_richness',
+        x='lexical_diversity',
         nbins=50, 
         title=f"Vocabulary Richness Distribution: {title}",
         color_discrete_sequence = get_color(label),
@@ -76,10 +79,10 @@ def vocab_richness(label):
     
 def article_length(label):
     # Article length distribution
-    temp_df = analysis_df[analysis_df['label'] == label]
+    temp_df = readability_metrics_df[readability_metrics_df['label'] == label]
     fig_length = px.histogram(
         temp_df, 
-        x='text_length', 
+        x='sentence_count', 
         color_discrete_sequence = get_color(label),
         nbins=50, 
         title="Article Length Distribution by Label",
@@ -87,15 +90,14 @@ def article_length(label):
     st.plotly_chart(fig_length)
     
 def n_grams(label, ngram_size):
+    print(n_grams_df.columns)
     if label == 0:
         temp_df = n_grams_df[n_grams_df['category'] == 'real']
     else:
         temp_df = n_grams_df[n_grams_df['category'] == 'fake']
 
-    # Filter data based on n-gram size
     filtered_data = temp_df[temp_df['ngram_size'] == ngram_size]
 
-    # Create a bar plot
     fig = px.bar(
         filtered_data,
         x='ngram',
@@ -106,7 +108,6 @@ def n_grams(label, ngram_size):
         height=600,
     )
 
-    # Update layout for better display
     fig.update_layout(
         xaxis_title="N-gram",
         yaxis_title="Frequency",
@@ -115,7 +116,6 @@ def n_grams(label, ngram_size):
         yaxis_tickfont=dict(size=10),
     )
 
-    # Display plot
     st.plotly_chart(fig)
 
 col_1, col_2, col_3 = st.columns([2, 1, 2])
@@ -123,29 +123,28 @@ with col_2:
     st.title("Dataset Analysis")
     st.subheader("Real vs. Fake News")
 
-# Split the page into two columns
 real_news_col, fake_news_col = st.columns(2)
 
 with real_news_col:
-    render_svg_inline(real_news_svg_content, "Real News WordCloud", 0)
+    render_svg_inline(svg_dict["real"], "Real News WordCloud", 0)
 
 with fake_news_col:
-    render_svg_inline(fake_news_svg_content, "Fake News WordCloud", 1)
+    render_svg_inline(svg_dict["fake"], "Fake News WordCloud", 1)
 
 col_1, col_2, col_3 = st.columns([1, 2, 1])
 with col_2:
     st.title("N-grams and Most Frequent Words")
     ngram_size = st.slider("Select n-gram size:", min_value=1, max_value=4, value=1)
+    
+    
 real_news_col_2, fake_news_col_2 = st.columns(2)
-with real_news_col_2:
 
+with real_news_col_2:
     n_grams(0, ngram_size)
     vocab_richness(0)
     article_length(0)
 
-# Add Fake News WordCloud on the right
 with fake_news_col_2:
-
     n_grams(1, ngram_size)
     vocab_richness(1)
     article_length(1)
